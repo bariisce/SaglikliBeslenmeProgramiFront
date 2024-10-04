1b3672766f55f2e4ee54fc51d13934b6d2451630
mgeee35 <mgeee35@proton.me>
2024-10-04 14:27:30 +0300

    Allergies page added to patient/allergies section.

--- src/app/views/patient/allergies/allergies.component.ts
+++ src/app/views/patient/allergies/allergies.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+// import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-allergies',
   standalone: true,
   imports: [],
   templateUrl: './allergies.component.html',
-  styleUrl: './allergies.component.scss'
+  styleUrl: './allergies.component.scss',
+  // schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AllergiesComponent {
 
